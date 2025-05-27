b87a4248db604e3345ebd2cb5af4c2f43bc02c15
yoodain1027 <yoodain1027@naver.com>
2025-05-27 17:38:14 +0900

    Update modeluser.js

--- modeluser.js
+++ modeluser.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "yourpassword",
-    database: "yourdatabase"
+    password: "1234",
+    database: "yoodain"
 });
 
 async function createUserTable() {
